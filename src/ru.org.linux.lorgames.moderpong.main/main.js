cfc2e1997aec071fd6d67bdd702ba71dcfc5f258
Oleg Chiruhin <hedin.pr@gmail.com>
2012-01-18 22:23:23 +0700

    add ability to stretch JB and implement collision detection

--- src/ru.org.linux.lorgames.moderpong.main/main.js
+++ src/ru.org.linux.lorgames.moderpong.main/main.js
@@ -1,31 +1,31 @@
 var moderpong = {};
 moderpong.pressedKeys = [];
 moderpong.moderhead = {
-    speed: 5,
-    x: 150,
-    y: 100,
-    directionX: 1,
-    directionY: 1
+    speed:5,
+    x:150,
+    y:100,
+    directionX:1,
+    directionY:1
 }
 
 var KEY = {
-    UP: 38,
-    DOWN: 40,
-    W: 87,
-    S: 83
+    UP:38,
+    DOWN:40,
+    W:87,
+    S:83
 }
 
-$(function(){
+$(function () {
     main();
 });
 
 function main() {
-    moderpong.timer = setInterval(gameloop,30);
+    moderpong.timer = setInterval(gameloop, 30);
 
-    $(document).keydown(function(e){
+    $(document).keydown(function (e) {
         moderpong.pressedKeys[e.which] = true;
     });
-    $(document).keyup(function(e){
+    $(document).keyup(function (e) {
         moderpong.pressedKeys[e.which] = false;
     });
 }
@@ -39,19 +39,19 @@ function gameloop() {
 function movePaddles() {
     if (moderpong.pressedKeys[KEY.UP]) {
         var top = parseInt($("#paddleB").css("top"));
-        $("#paddleB").css("top",top-5);
+        $("#paddleB").css("top", top - 5);
     }
     if (moderpong.pressedKeys[KEY.DOWN]) {
         var top = parseInt($("#paddleB").css("top"));
-        $("#paddleB").css("top",top+5);
+        $("#paddleB").css("top", top + 5);
     }
     if (moderpong.pressedKeys[KEY.W]) {
         var top = parseInt($("#paddleA").css("top"));
-        $("#paddleA").css("top",top-5);
+        $("#paddleA").css("top", top - 5);
     }
     if (moderpong.pressedKeys[KEY.S]) {
         var top = parseInt($("#paddleA").css("top"));
-        $("#paddleA").css("top",top+5);
+        $("#paddleA").css("top", top + 5);
     }
 }
 
@@ -63,30 +63,66 @@ function moveModerhead() {
 
     // check playground boundary
     // check bottom edge
-    if (moderhead.y + moderhead.speed*moderhead.directionY > playgroundHeight)
-    {
+    if (moderhead.y + moderhead.speed * moderhead.directionY > playgroundHeight) {
         moderhead.directionY = -1;
     }
     // check top edge
-    if (moderhead.y + moderhead.speed*moderhead.directionY < 0)
-    {
+    if (moderhead.y + moderhead.speed * moderhead.directionY < 0) {
         moderhead.directionY = 1;
     }
+
     // check right edge
-    if (moderhead.x + moderhead.speed*moderhead.directionX > playgroundWidth)
-    {
+    if (moderhead.x + moderhead.speed * moderhead.directionX > playgroundWidth) {
+        // player B lost.    
+        // reset the moderhead;
+        moderhead.x = 250;
+        moderhead.y = 100;
+        $("#moderhead").css({
+            "left":moderhead.x,
+            "top":moderhead.y
+        });
         moderhead.directionX = -1;
     }
+
     // check left edge
-    if (moderhead.x + moderhead.speed*moderhead.directionX < 0)
-    {
+    if (moderhead.x + moderhead.speed * moderhead.directionX < 0) {
+        // player A lost.    
+        // reset the moderhead;
+        moderhead.x = 150;
+        moderhead.y = 100;
+        $("#moderhead").css({
+            "left":moderhead.x,
+            "top":moderhead.y
+        });
         moderhead.directionX = 1;
     }
+
     moderhead.x += moderhead.speed * moderhead.directionX;
     moderhead.y += moderhead.speed * moderhead.directionY;
 
+    // check left paddle
+    var paddleAX = parseInt($("#paddleA").css("left")) + parseInt($("#paddleA").css("width"));
+    var paddleAYBottom = parseInt($("#paddleA").css("top")) + parseInt($("#paddleA").css("height"));
+    var paddleAYTop = parseInt($("#paddleA").css("top"));
+    if (moderhead.x + moderhead.speed * moderhead.directionX < paddleAX) {
+        if (moderhead.y + moderhead.speed * moderhead.directionY <= paddleAYBottom &&
+            moderhead.y + moderhead.speed * moderhead.directionY >= paddleAYTop) {
+            moderhead.directionX = 1;
+        }
+    }
+    // check right paddle
+    var paddleBX = parseInt($("#paddleB").css("left"));
+    var paddleBYBottom = parseInt($("#paddleB").css("top")) + parseInt($("#paddleB").css("height"));
+    var paddleBYTop = parseInt($("#paddleB").css("top"));
+    if (moderhead.x + moderhead.speed * moderhead.directionX >= paddleBX) {
+        if (moderhead.y + moderhead.speed * moderhead.directionY <= paddleBYBottom &&
+            moderhead.y + moderhead.speed * moderhead.directionY >= paddleBYTop) {
+            moderhead.directionX = -1;
+        }
+    }
+
     $("#moderhead").css({
-        "left" : moderhead.x,
-        "top" : moderhead.y
+        "left":moderhead.x,
+        "top":moderhead.y
     });
 }
